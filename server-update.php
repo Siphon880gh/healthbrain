90e21c799a6371859e9081531d4316f5ad2a75b3
Siphon880gh <weng.f.fung@gmail.com>
2024-02-27 16:34:14 -0800

    Content update

--- server-update.php
+++ server-update.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $command1 = 'cd "/home/bse7iy70lkjz/public_html/tools/3dbrain/curriculum";';
-$command2 = 'git fetch origin; git reset --hard refs/remotes/origin/main';
+$command2 = 'git fetch origin; git reset --hard refs/remotes/origin/main;';
 $command = $command1 . $command2;
 $output = shell_exec("$command 2>&1");
 $exitCode = shell_exec('echo $?');
